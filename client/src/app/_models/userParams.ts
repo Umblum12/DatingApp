3cf016ea578ea9e662bebbf224101210a16af8d5
Umblum12 <abelardo_cruzleos@hotmail.com>
2023-05-16 12:18:47 -0600

    Final de la seccion 13

--- client/src/app/_models/userParams.ts
+++ client/src/app/_models/userParams.ts
@@ -8,7 +8,6 @@ export class UserParams {
     pageSize = 5;
     orderBy = "lastActive";
 
-
     constructor(user: User) {
         this.gender = user.gender === 'female' ? 'male' : 'female'
         
